bd8bfdf2fa244735147f22f86d9ea32102e89a15
michaelkircos <32113238+michaelkircos@users.noreply.github.com>
2021-11-16 21:24:11 -0600

    My 1st attempt at the panic.py challenge exercise.

---
+++ panic.py
@@ -0,0 +1,17 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+print(phrase)
+print(plist)
+
+plist.pop(0)    ## Removes the 'D'
+for p in range(9)
+    plist.pop(2)    ## Removes everything to the right of 'on'
+plist.extend(' ')
+plist.extend('t')
+plist.extend('a')
+plist.extend('p')    
+
+
+new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
